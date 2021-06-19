febcc4345d65f5694375d9bacbaee7fc8358b193
Kazuki Ono <kazukitono5252.com@icloud.com>
2021-06-20 01:05:37 +0900

    masterから新しく切ってボタンを作りました

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,8 @@
+atoms-button.stories.js
+
+import Item_Button from "../components/atoms/button/button.html";
+import "../components/atoms/button/button.scss";
+
+export default { title: "Atoms/Button" }
+
+export const Button = () => Item_Button;
